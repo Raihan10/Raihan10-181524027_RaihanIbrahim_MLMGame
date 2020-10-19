482b6376d2dcaf97532a277e196e49c26dff8d65
Raihan10 <50223627+Raihan10@users.noreply.github.com>
2020-10-19 08:09:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.get('/', function(req, res){
 });
 
 app.post('/add',function(req, res){
+    client.query("SELECT setval('recipes_id_seq', COALESCE((SELECT MAX(id)+1 FROM recipes), 1), false)");
     client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)',
         [req.body.name, req.body.ingredients, req.body.directions]);
         res.redirect('/');
@@ -62,11 +63,13 @@ app.post('/add',function(req, res){
 
 app.delete('/delete/:id', function(req, res){
     client.query('DELETE FROM recipes WHERE id = $1',
-        [req.params.id]);
+        [req.params.id]);    
+    client.query("SELECT setval('recipes_id_seq', COALESCE((SELECT MAX(id)+1 FROM recipes), 1), false)");
         res.send(200);
 })
 
 app.post('/edit', function(req, res) {
+    client.query("SELECT setval('recipes_id_seq', COALESCE((SELECT MAX(id)+1 FROM recipes), 1), false)");
     client.query('UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4',
         [req.body.name, req.body.ingredients, req.body.directions, req.body.id]);
         res.redirect('/');
